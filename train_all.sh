734d896c14efd2b18b0e4e5cc297fa0b39b3b6ed
amitay.s <amitay.sich@gmail.com>
2024-09-26 13:53:01 +0300

    start

--- train_all.sh
+++ train_all.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-#SBATCH --time=3-00
-#SBATCH --array=1-3
+#SBATCH --time=7-00
+#SBATCH --array=1-4
 #SBATCH --mem=64G
 #SBATCH --requeue
 #SBATCH --gres=gpu:A40:1
@@ -14,7 +14,10 @@ case $SLURM_ARRAY_TASK_ID in
     python train.py --use_ec 1 --ec_split 1
     ;;
   3)
-    python train.py --use_ec 1 --ec_split 0
+    python train.py --use_ec 1 --ec_split 0 --lookup_len 1
+    ;;
+  4)
+    python train.py --use_ec 1 --ec_split 0 --lookup_len 5
     ;;
   *)
     echo "Invalid SLURM_ARRAY_TASK_ID: $SLURM_ARRAY_TASK_ID"
