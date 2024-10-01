a79bf2dadda9dcab8ec6a72e20f0db5dd9367c9b
amitay.s <amitay.sich@gmail.com>
2024-10-01 11:50:20 +0300

    start

--- preprocessing/prep_docking.py
+++ preprocessing/prep_docking.py
@@ -5,13 +5,16 @@ import os
 import rdkit.rdBase as rkrb
 import rdkit.RDLogger as rkl
 from tqdm import tqdm
+
 logger = rkl.logger()
 logger.setLevel(rkl.ERROR)
 rkrb.DisableLog("rdApp.error")
 ec_mapping = pd.read_csv("datasets/ec_map.csv")
 ec_to_uniport = defaultdict(str)
+ec_to_fasta = defaultdict(str)
 for i, row in ec_mapping.iterrows():
     ec_to_uniport[row["EC_full"]] = row["Uniprot_id"]
+    ec_to_fasta[row["EC_full"]] = row["Sequence"]
 
 base_dataset = "datasets/ecreact/ecreact-1.0.txt"
 with open(base_dataset) as f:
@@ -36,14 +39,20 @@ for line in tqdm(lines):
         if name in all_names:
             continue
         all_names.add(name)
-        results.append((name, s, f'../BioCatalystChem/datasets/pdb_files/{uniprot_id}/{uniprot_id}_esmfold.pdb'))
-results_df = pd.DataFrame(results, columns=["complex_name", "ligand_description", "protein_path"])
+        pdb_file = f"../BioCatalystChem/datasets/pdb_files/{uniprot_id}/{uniprot_id}_esmfold.pdb"
+        pdb_seq = ""
+        if not os.path.exists(pdb_file):
+            pdb_file = ""
+            pdb_seq = ec_to_fasta[ec]
+
+        results.append((name, s, pdb_file, pdb_seq))
+results_df = pd.DataFrame(results, columns=["complex_name", "ligand_description", "protein_path", "protein_sequence"])
 output_pdb_dir = "datasets/docking"
 if not os.path.exists(output_pdb_dir):
     os.makedirs(output_pdb_dir)
-n_splits = 8
+n_splits = 20
 for i in range(n_splits):
     results_df.iloc[i::n_splits].to_csv(f"{output_pdb_dir}/split_{i + 1}.csv", index=False)
-with open(f"{output_pdb_dir}/smiles_to_id.txt",'w') as f:
+with open(f"{output_pdb_dir}/smiles_to_id.txt", 'w') as f:
     for k, v in smiles_to_id.items():
         f.write(f"{k} {v}\n")
