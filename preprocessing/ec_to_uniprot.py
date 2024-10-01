cc1e88fddfa6d802e0e67903d93c6a8308ab585c
amitay.s <amitay.sich@gmail.com>
2024-10-01 05:21:16 +0300

    start

--- preprocessing/ec_to_uniprot.py
+++ preprocessing/ec_to_uniprot.py
@@ -14,77 +14,78 @@ device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cp
 
 
 
-def convert_outputs_to_pdb(outputs):
-    final_atom_positions = atom14_to_atom37(outputs["positions"][-1], outputs)
-    outputs = {k: v.to("cpu").numpy() for k, v in outputs.items()}
-    final_atom_positions = final_atom_positions.cpu().numpy()
-    final_atom_mask = outputs["atom37_atom_exists"]
-    pdbs = []
-    for i in range(outputs["aatype"].shape[0]):
-        aa = outputs["aatype"][i]
-        pred_pos = final_atom_positions[i]
-        mask = final_atom_mask[i]
-        resid = outputs["residue_index"][i] + 1
-        pred = OFProtein(
-            aatype=aa,
-            atom_positions=pred_pos,
-            atom_mask=mask,
-            residue_index=resid,
-            b_factors=outputs["plddt"][i],
-            chain_index=outputs["chain_index"][i] if "chain_index" in outputs else None,
-        )
-        pdbs.append(to_pdb(pred))
-    return pdbs
-
-
-
-
-class ESMFold:
-    def __init__(self):
-        self.tokenizer = AutoTokenizer.from_pretrained("facebook/esmfold_v1")
-        self.model = EsmForProteinFolding.from_pretrained("facebook/esmfold_v1", low_cpu_mem_usage=True)
-        self.model.to(device)
-        self.model.esm = self.model.esm.half()
-        torch.backends.cuda.matmul.allow_tf32 = True
-        self.chunk_size = 256
-        self.model.trunk.set_chunk_size(self.chunk_size)
-
-    def fold(self, seq, output_file):
-        tokenized_input = self.tokenizer([seq], return_tensors="pt", add_special_tokens=False)['input_ids']
-        tokenized_input = tokenized_input.to(device)
-
-        output = None
-
-        while output is None:
-            try:
-                with torch.no_grad():
-                    output = self.model(tokenized_input)
-            except RuntimeError as e:
-                if 'out of memory' in str(e):
-                    print('| WARNING: ran out of memory on chunk_size', self.chunk_size)
-                    for p in self.model.parameters():
-                        if p.grad is not None:
-                            del p.grad  # free some memory
-                    torch.cuda.empty_cache()
-                    self.chunk_size = self.chunk_size // 2
-                    if self.chunk_size > 2:
-                        self.model.trunk.set_chunk_size(self.chunk_size)
-                    else:
-                        print("Not enough memory for ESMFold")
-                        break
-                else:
-                    raise e
-        if output is None:
-            return
-        pdb_file = convert_outputs_to_pdb(output)[0]
-        with open(output_file, "w") as f:
-            f.write(pdb_file)
-
+# def convert_outputs_to_pdb(outputs):
+#     final_atom_positions = atom14_to_atom37(outputs["positions"][-1], outputs)
+#     outputs = {k: v.to("cpu").numpy() for k, v in outputs.items()}
+#     final_atom_positions = final_atom_positions.cpu().numpy()
+#     final_atom_mask = outputs["atom37_atom_exists"]
+#     pdbs = []
+#     for i in range(outputs["aatype"].shape[0]):
+#         aa = outputs["aatype"][i]
+#         pred_pos = final_atom_positions[i]
+#         mask = final_atom_mask[i]
+#         resid = outputs["residue_index"][i] + 1
+#         pred = OFProtein(
+#             aatype=aa,
+#             atom_positions=pred_pos,
+#             atom_mask=mask,
+#             residue_index=resid,
+#             b_factors=outputs["plddt"][i],
+#             chain_index=outputs["chain_index"][i] if "chain_index" in outputs else None,
+#         )
+#         pdbs.append(to_pdb(pred))
+#     return pdbs
+#
+#
+#
+#
+# class ESMFold:
+#     def __init__(self):
+#         self.tokenizer = AutoTokenizer.from_pretrained("facebook/esmfold_v1")
+#         self.model = EsmForProteinFolding.from_pretrained("facebook/esmfold_v1", low_cpu_mem_usage=True)
+#         self.model.to(device)
+#         self.model.esm = self.model.esm.half()
+#         torch.backends.cuda.matmul.allow_tf32 = True
+#         self.chunk_size = 256
+#         self.model.trunk.set_chunk_size(self.chunk_size)
+#
+#     def fold(self, seq, output_file):
+#         tokenized_input = self.tokenizer([seq], return_tensors="pt", add_special_tokens=False)['input_ids']
+#         tokenized_input = tokenized_input.to(device)
+#
+#         output = None
+#
+#         while output is None:
+#             try:
+#                 with torch.no_grad():
+#                     output = self.model(tokenized_input)
+#             except RuntimeError as e:
+#                 if 'out of memory' in str(e):
+#                     print('| WARNING: ran out of memory on chunk_size', self.chunk_size)
+#                     for p in self.model.parameters():
+#                         if p.grad is not None:
+#                             del p.grad  # free some memory
+#                     torch.cuda.empty_cache()
+#                     self.chunk_size = self.chunk_size // 2
+#                     if self.chunk_size > 2:
+#                         self.model.trunk.set_chunk_size(self.chunk_size)
+#                     else:
+#                         print("Not enough memory for ESMFold")
+#                         break
+#                 else:
+#                     raise e
+#         if output is None:
+#             return
+#         pdb_file = convert_outputs_to_pdb(output)[0]
+#         with open(output_file, "w") as f:
+#             f.write(pdb_file)
+#
 
 @dataclass
 class Protein:
     ec_full: str
     ec_use: str
+    uniprot_id: str
     seq: str
     pdb_id: str
     pdb_file: str
@@ -141,7 +142,7 @@ def protein_to_dataframes(proteins: List[Protein]):
 
 if __name__ == "__main__":
     uniprot = UniProt()
-    esm_fold = ESMFold()
+    # esm_fold = ESMFold()
     output_dir = "datasets/pdb_files"
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
@@ -157,14 +158,17 @@ if __name__ == "__main__":
         id_, fasta, ec_use = ec_to_id_fasta(ec)
         if id_ == "":
             continue
-        pdb_id = uniprot_id_to_pdb_id(id_)
-        if pdb_id != "":
-            pdb_file = get_pdb_file(pdb_id, output_dir)
-        else:
-            pdb_file = ""
-        if pdb_file == "":
-            pdb_file = f"{output_dir}/{id_}.pdb"
-            esm_fold.fold(fasta, pdb_file)
-        proteins.append(Protein(ec, ec_use, fasta, pdb_id, pdb_file))
+        # pdb_id = uniprot_id_to_pdb_id(id_)
+        # if pdb_id != "":
+        #     pdb_file = get_pdb_file(pdb_id, output_dir)
+        # else:
+        #     pdb_file = ""
+        # if pdb_file == "":
+        #     pdb_file = f"{output_dir}/{id_}.pdb"
+        #     esm_fold.fold(fasta, pdb_file)
+        proteins.append(Protein(ec, ec_use,id_, fasta, "", ""))
     df = protein_to_dataframes(proteins)
     df.to_csv(f"datasets/ec_map.csv", index=False)
+    protein_ligend_for_pdbs = [(p.uniprot_id,p.seq,"O") for p in proteins]
+    df = pd.DataFrame(protein_ligend_for_pdbs, columns=["complex_name", "protein_sequence", "ligand_description"])
+    df.to_csv("datasets/protein_ligand_to_pdb.csv", index=False)
