23ae5fd31b7f88d4ba8e9519db9e4b19929acdd5
amitay.s <amitay.sich@gmail.com>
2024-10-07 14:47:55 +0300

    start

---
+++ gen_all.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+#SBATCH --time=1-00
+#SBATCH --array=1-8
+#SBATCH --mem=64G
+#SBATCH --requeue
+#SBATCH --gres=gpu:A4000:1
+
+# dae_1_add  dae_1_seq  dae_5_add  dae_5_seq  paper  pretrained_1  pretrained_5  regular
+ case $SLURM_ARRAY_TASK_ID in
+  1)
+    python eval_gen.py --run_name dae_1_add
+    ;;
+  2)
+    python eval_gen.py --run_name dae_1_seq
+    ;;
+  3)
+    python eval_gen.py --run_name dae_5_add
+    ;;
+  4)
+    python eval_gen.py --run_name dae_5_seq
+    ;;
+  5)
+    python eval_gen.py --run_name paper
+    ;;
+  6)
+    python eval_gen.py --run_name pretrained_1
+    ;;
+  7)
+    python eval_gen.py --run_name pretrained_5
+    ;;
+  8)
+    python eval_gen.py --run_name regular
+    ;;
+  *)
+    echo "Invalid SLURM_ARRAY_TASK_ID: $SLURM_ARRAY_TASK_ID"
+    ;;
+  esac
+# End of file
