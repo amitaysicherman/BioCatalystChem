6d03240882b68be408a7efdb15435017869f30e8
amitay.s <amitay.sich@gmail.com>
2024-10-09 10:20:28 +0300

    start

--- train_all.sh
+++ train_all.sh
@@ -8,19 +8,19 @@
 # Run different script based on SLURM_ARRAY_TASK_ID
 case $SLURM_ARRAY_TASK_ID in
   1)
-    python train.py --use_ec 1 --ec_split 1 --load_cp "results/regular/checkpoint-280000" --ecreact_only 1
+    python train.py --use_ec 1 --ec_split 1 --load_cp "results_old/v4/regular/checkpoint-280000" --ecreact_only 1
     ;;
   2)
-    python train.py --use_ec 1 --ec_split 0 --lookup_len 1 --load_cp "results/regular/checkpoint-280000" --ecreact_only 1
+    python train.py --use_ec 1 --ec_split 0 --lookup_len 1 --load_cp "results_old/v4/regular/checkpoint-280000" --ecreact_only 1
     ;;
   3)
-    python train.py --use_ec 1 --ec_split 0 --lookup_len 5 --load_cp "results/regular/checkpoint-280000" --ecreact_only 1
+    python train.py --use_ec 1 --ec_split 0 --lookup_len 5 --load_cp "results_old/v4/regular/checkpoint-280000" --ecreact_only 1
     ;;
   4)
-    python train.py --dae 1 --ec_split 1 --lookup_len 1 --load_cp "results/regular/checkpoint-280000" --ecreact_only 1
+    python train.py --dae 1 --ec_split 1 --lookup_len 1 --load_cp "results_old/v4/regular/checkpoint-280000" --ecreact_only 1
     ;;
   5)
-    python train.py --dae 1  --ec_split 1 --lookup_len 5 --load_cp "results/regular/checkpoint-280000" --ecreact_only 1
+    python train.py --dae 1  --ec_split 1 --lookup_len 5 --load_cp "results_old/v4/regular/checkpoint-280000" --ecreact_only 1
     ;;
   *)
     echo "Invalid SLURM_ARRAY_TASK_ID: $SLURM_ARRAY_TASK_ID"
