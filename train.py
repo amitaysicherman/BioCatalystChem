ab71dcc111c2d46f77da579a31b19a8752f65381
amitay.s <amitay.sich@gmail.com>
2024-10-23 01:12:10 +0300

    start

--- train.py
+++ train.py
@@ -133,7 +133,8 @@ def main(use_ec=True, ec_split=False, lookup_len=5, dae=False, load_cp="", ecrea
         loaded_state_dict = load_file(load_cp + "/model.safetensors")
         if prequantization:
             new_tokens_count = tokenizer.vocab_size - last_original_token
-            random_init_new_tokens_param = torch.randn(new_tokens_count, model.t5_model.config.d_model)
+            d_model= model.t5_model.config.d_model if isinstance(model, EnzymaticT5Model) else model.config.d_model
+            random_init_new_tokens_param = torch.randn(new_tokens_count, d_model)
             new_shared = torch.cat([loaded_state_dict["shared"], random_init_new_tokens_param], dim=0)
             loaded_state_dict["shared"] = new_shared
         if isinstance(model, EnzymaticT5Model):
