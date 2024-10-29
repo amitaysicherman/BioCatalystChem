b2026a3f15c09a71a18a4540d51cb62b06432397
amitay.s <amitay.sich@gmail.com>
2024-10-29 17:50:05 +0200

    start

--- eval_gen.py
+++ eval_gen.py
@@ -161,7 +161,7 @@ if __name__ == "__main__":
         model = T5ForConditionalGeneration.from_pretrained(best_val_cp)
     else:
         model = CustomT5Model.from_pretrained(best_val_cp, lookup_len=lookup_len)
-    gen_dataset = SeqToSeqDataset([ecreact_dataset], args.split, tokenizer=tokenizer, ec_type=ec_type, DEBUG=False)
+    gen_dataset = SeqToSeqDataset([ecreact_dataset], args.split, tokenizer=tokenizer, ec_type=ec_type, DEBUG=False,sample_size=50)
     gen_dataloader = DataLoader(gen_dataset, batch_size=1, num_workers=0)
 
     model.to(device)
