413a1722d74141b79f6c2a49f95a25fd08567216
amitay.s <amitay.sich@gmail.com>
2024-10-31 17:07:54 +0200

    start

--- finetune_ecreact.py
+++ finetune_ecreact.py
@@ -173,7 +173,7 @@ def main(ec_type, lookup_len, prequantization, n_hierarchical_clusters, n_pca_co
         report_to='none' if DEBUG else 'tensorboard',
 
         run_name=run_name,
-        learning_rate=1e-3,
+        learning_rate=1e-5,
 
         save_safetensors=False
 
