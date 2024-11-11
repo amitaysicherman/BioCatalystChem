4a011e0f8a345c4d6a64fbb312664a3ec6b43e60
amitay.s <amitay.sich@gmail.com>
2024-11-11 11:17:24 +0200

    start

--- train_all.sh
+++ train_all.sh
@@ -4,7 +4,6 @@
 #SBATCH --requeue
 #SBATCH --gres=gpu:A40:1
 #SBATCH -c 10
-#SBATCH -w newton3,newton4
 #SBATCH --array=1-2
 
 # Define the configurations as a long string with a delimiter (| in this case)
