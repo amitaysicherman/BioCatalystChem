784c67f31e0bbbd1c98cb2d8a04abfdb867fbefa
amitay.s <amitay.sich@gmail.com>
2024-11-17 18:00:43 +0200

    start

--- finetune_ecreact.py
+++ finetune_ecreact.py
@@ -226,7 +226,7 @@ def main(ec_type, lookup_len, prequantization, n_hierarchical_clusters, n_pca_co
     # Training arguments
     output_dir = f"results/{run_name}"
     if not mix:
-        num_train_epochs = 20
+        num_train_epochs = 25
     else:
         num_train_epochs = 10
 
