8754d3831d53aa318a54b0077622c2ff7d9ba57c
amitay.s <amitay.sich@gmail.com>
2024-11-17 19:52:11 +0200

    start

--- finetune_ecreact.py
+++ finetune_ecreact.py
@@ -202,7 +202,9 @@ def main(ec_type, lookup_len, prequantization, n_hierarchical_clusters, n_pca_co
     train_small_dataset = SeqToSeqDataset([ecreact_dataset], "train", **common_ds_args, sample_size=1000, **dup_args)
     val_small_dataset = SeqToSeqDataset([ecreact_dataset], "valid", **common_ds_args, **dup_args)
     test_small_dataset = SeqToSeqDataset([ecreact_dataset], "test", **common_ds_args, sample_size=1000, **dup_args)
-    test_uspto_dataset = SeqToSeqDataset(["uspto"], "test", **common_ds_args, sample_size=1000)
+    uspto_args= {**common_ds_args}
+    uspto_args['ec_source'] = None
+    test_uspto_dataset = SeqToSeqDataset(["uspto"], "test", **uspto_args, sample_size=1000)
 
     eval_datasets = {"train": train_small_dataset, "valid": val_small_dataset, "test": test_small_dataset,
                      "uspto": test_uspto_dataset}
