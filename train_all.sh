8a86423f005880f518ce07aea786b6e7b564835a
amitay.s <amitay.sich@gmail.com>
2024-11-19 21:35:11 +0200

    start

--- train_all.sh
+++ train_all.sh
@@ -2,16 +2,15 @@
 #SBATCH --time=7-00
 #SBATCH --mem=128G
 #SBATCH --requeue
-#SBATCH --gres=gpu:A40:1
+#SBATCH --gres=gpu:1
 #SBATCH -c 8
+#SBATCH -w bruno1,bruno2,euler1,dym-lab,dym-lab2,galileo1,galileo2,newton3,newton4,nlp-A40-1,newton2,newton5,nlp-L40-1,nlp-L40-2,tdk-bm4,bruno3
 #SBATCH --array=1-14
 
 configs="--ec_type 0  --max_length 200 |\
   --ec_type 1  --max_length 205  |\
   --ec_type 2  --max_length 200  |\
   --ec_type 3  --max_length 200  |\
-  --ec_type 2  --addec 1 --max_length 205  |\
-  --ec_type 3  --addec 1 --max_length 205  |\
   --ec_type 2 --prequantization 1 --n_hierarchical_clusters 7 --n_pca_components 0 --n_clusters_pca 7 --addec 1 --max_length 213  |\
   --ec_type 3 --prequantization 1 --n_hierarchical_clusters 7 --n_pca_components 0 --n_clusters_pca 7 --addec 1 --max_length 213 |\
   --ec_type 2 --prequantization 1 --n_hierarchical_clusters 7 --n_pca_components 0 --n_clusters_pca 7  --max_length 208  |\
