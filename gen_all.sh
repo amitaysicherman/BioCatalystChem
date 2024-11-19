e3f8b956012d3b2a5ca324e0aeb42180b0d9e460
amitay.s <amitay.sich@gmail.com>
2024-11-19 17:56:59 +0200

    start

--- gen_all.sh
+++ gen_all.sh
@@ -13,10 +13,10 @@ RUN_NAMES=($(ls -d $RESULTS_DIR*/ | xargs -n 1 basename))
 # Check if the SLURM_ARRAY_TASK_ID is within the bounds of the available run names
 if [ $SLURM_ARRAY_TASK_ID -le ${#RUN_NAMES[@]} ]; then\
     RUN_NAME=${RUN_NAMES[$SLURM_ARRAY_TASK_ID - 1]} # Arrays are 0-indexed
-    if [[ $RUN_NAME == *"mix"* ]]; then
-        echo "Skipping run: $RUN_NAME (contains 'mix')"
-        exit 0
-    fi
+#    if [[ $RUN_NAME == *"mix"* ]]; then
+#        echo "Skipping run: $RUN_NAME (contains 'mix')"
+#        exit 0
+#    fi
     python eval_gen.py --run_name $RUN_NAME --fast 0 --split test --dups 0 --per_level 1 --per_ds 1 --res_base ${RESULTS_DIR}  --bs 16
     python eval_gen.py --run_name $RUN_NAME --fast 0 --split test --dups 0 --per_level 1 --res_base ${RESULTS_DIR}  --bs 16
     python eval_gen.py --run_name $RUN_NAME --fast 0 --split test --dups 0 --per_level 0 --res_base ${RESULTS_DIR}  --bs 16  --only_new 1
