a8662ec1a8d1f6259c3934e2678a49b0bfbb8d3b
amitay.s <amitay.sich@gmail.com>
2024-11-19 21:28:51 +0200

    start

--- train_all.sh
+++ train_all.sh
@@ -21,25 +21,8 @@ configs="--ec_type 0  --max_length 200 |\
   --ec_type 2 --prequantization 1 --n_hierarchical_clusters 0 --n_pca_components 7 --n_clusters_pca 7  --max_length 208  |\
   --ec_type 3 --prequantization 1 --n_hierarchical_clusters 0 --n_pca_components 7 --n_clusters_pca 7  --max_length 208"
 
-# Set the GPU memory fraction
-tasks_on_gpu=6
-
 # Split the config string into an array using '|' as a delimiter
 IFS='|' read -ra config_array <<< "$configs"
 
-# Calculate the starting index for this job based on SLURM_ARRAY_TASK_ID
-start_index=$(( (SLURM_ARRAY_TASK_ID - 1) * tasks_on_gpu ))
-end_index=$(( start_index + tasks_on_gpu ))
-
-# Extract the subset of configs for this specific array job
-selected_configs=("${config_array[@]:start_index:tasks_on_gpu}")
-
-# Process each selected configuration
-for config in "${selected_configs[@]}"; do
-    {
-        python finetune_ecreact.py $config --tasks_on_gpu $tasks_on_gpu
-    } &
-done
-
-# Wait for all background jobs to finish
-wait
+config=${config_array[$((SLURM_ARRAY_TASK_ID - 1))]}
+python finetune_ecreact.py $config
