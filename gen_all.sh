7fe33526d5eab9990f710a49734141633ac308de
amitay.s <amitay.sich@gmail.com>
2024-11-19 12:26:08 +0200

    start

--- gen_all.sh
+++ gen_all.sh
@@ -7,7 +7,7 @@
 #SBATCH --requeue
 
 # Get the list of directories in results/
-RESULTS_DIR="results_work_version/"
+RESULTS_DIR="results/"
 RUN_NAMES=($(ls -d $RESULTS_DIR*/ | xargs -n 1 basename))
 
 # Check if the SLURM_ARRAY_TASK_ID is within the bounds of the available run names
