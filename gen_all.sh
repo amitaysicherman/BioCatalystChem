ebc8fe8ad468aaffc55bb4942fb8a86ba7f58366
amitay.s <amitay.sich@gmail.com>
2024-11-19 12:09:46 +0200

    start

--- gen_all.sh
+++ gen_all.sh
@@ -15,7 +15,7 @@ if [ $SLURM_ARRAY_TASK_ID -le ${#RUN_NAMES[@]} ]; then\
     RUN_NAME=${RUN_NAMES[$SLURM_ARRAY_TASK_ID - 1]} # Arrays are 0-indexed
     if [[ $RUN_NAME == *"mix"* ]]; then
         echo "Skipping run: $RUN_NAME (contains 'mix')"
-        continue
+        exit 0
     fi
     python eval_gen.py --run_name $RUN_NAME --fast 0 --split test --dups 0 --per_level 1 --per_ds 1 --res_base ${RESULTS_DIR}  --bs 16
 else
