5d2d45f19c1a845c6762f1348f07cb23d2e2f2b1
amitay.s <amitay.sich@gmail.com>
2024-11-20 13:10:01 +0200

    start

--- preprocessing/prep_docking_input_csv.py
+++ preprocessing/prep_docking_input_csv.py
@@ -84,6 +84,7 @@ if __name__ == "__main__":
     n_splits = 10
     split_size = len(df) // n_splits
     output_base = "../DiffDock/input_csvs"
+    os.makedirs(output_base, exist_ok=True)
     for i in range(n_splits):
         df.iloc[i * split_size:(i + 1) * split_size].to_csv(f"{output_base}/protein_ligand_{i}.csv", index=False)
 
