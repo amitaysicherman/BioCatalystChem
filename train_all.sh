f6684808bb793bf471e148d5ea8f81ec7f264cdd
amitay.s <amitay.sich@gmail.com>
2024-11-22 16:09:39 +0200

    start

--- train_all.sh
+++ train_all.sh
@@ -2,18 +2,19 @@
 #SBATCH --time=7-00
 #SBATCH --mem=128G
 #SBATCH --requeue
-#SBATCH --gres=gpu:L40:1
+#SBATCH --gres=gpu:A40:1
 #SBATCH -c 8
 #SBATCH --array=1-6
 
-configs=" --ec_type 3  --max_length 205 --addec 1 --daev2 1  --alpha 50|\
-  --ec_type 3  --max_length 200 --daev2 1  --alpha 50|\
-  --ec_type 3  --max_length 205 --addec 1 --daev2 1 --alpha 10|\
-  --ec_type 3  --max_length 200 --daev2 1  --alpha 10|\
-  --ec_type 3  --max_length 205 --addec 1 --daev2 1  --alpha 90|\
-  --ec_type 3  --max_length 200 --daev2 1 --alpha 90"
+configs=" --ec_type 3  --max_length 213 --addec 1 --daev2 1  --alpha 50 --prequantization --n_hierarchical_clusters 7 --n_pca_components 0 --n_clusters_pca 7 |\
+  --ec_type 3  --max_length 208 --daev2 1  --alpha 50 --prequantization --n_hierarchical_clusters 7 --n_pca_components 0 --n_clusters_pca 7 |\
+  --ec_type 3  --max_length 213 --addec 1 --daev2 1 --alpha 10 --prequantization --n_hierarchical_clusters 7 --n_pca_components 0 --n_clusters_pca 7 |\
+  --ec_type 3  --max_length 208 --daev2 1  --alpha 10 --prequantization --n_hierarchical_clusters 7 --n_pca_components 0 --n_clusters_pca 7 |\
+  --ec_type 3  --max_length 213 --addec 1 --daev2 1  --alpha 90 --prequantization --n_hierarchical_clusters 7 --n_pca_components 0 --n_clusters_pca 7 |\
+  --ec_type 3  --max_length 208 --daev2 1 --alpha 90 --prequantization --n_hierarchical_clusters 7 --n_pca_components 0 --n_clusters_pca 7 "
 # Split the config string into an array using '|' as a delimiter
 IFS='|' read -ra config_array <<< "$configs"
 
 config=${config_array[$((SLURM_ARRAY_TASK_ID - 1))]}
+sleep 10h
 python finetune_ecreact.py $config
