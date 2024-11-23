a78721398045407374a894294c7a9c4d76c6e884
amitay.s <amitay.sich@gmail.com>
2024-11-23 19:35:22 +0200

    start

--- train_all.sh
+++ train_all.sh
@@ -6,12 +6,12 @@
 #SBATCH -c 8
 #SBATCH --array=1-6
 
-configs=" --ec_type 3  --max_length 213 --addec 1 --daev2 1  --alpha 50 --prequantization --n_hierarchical_clusters 7 --n_pca_components 0 --n_clusters_pca 7 |\
-  --ec_type 3  --max_length 208 --daev2 1  --alpha 50 --prequantization --n_hierarchical_clusters 7 --n_pca_components 0 --n_clusters_pca 7 |\
-  --ec_type 3  --max_length 213 --addec 1 --daev2 1 --alpha 10 --prequantization --n_hierarchical_clusters 7 --n_pca_components 0 --n_clusters_pca 7 |\
-  --ec_type 3  --max_length 208 --daev2 1  --alpha 10 --prequantization --n_hierarchical_clusters 7 --n_pca_components 0 --n_clusters_pca 7 |\
-  --ec_type 3  --max_length 213 --addec 1 --daev2 1  --alpha 90 --prequantization --n_hierarchical_clusters 7 --n_pca_components 0 --n_clusters_pca 7 |\
-  --ec_type 3  --max_length 208 --daev2 1 --alpha 90 --prequantization --n_hierarchical_clusters 7 --n_pca_components 0 --n_clusters_pca 7 "
+configs=" --ec_type 3  --max_length 213 --addec 1 --daev2 1  --alpha 50 --prequantization 1 prequantization 1 |\
+  --ec_type 3  --max_length 208 --daev2 1  --alpha 50 --prequantization 1 prequantization 1 |\
+  --ec_type 3  --max_length 213 --addec 1 --daev2 1 --alpha 10 --prequantization 1 prequantization 1 |\
+  --ec_type 3  --max_length 208 --daev2 1  --alpha 10 --prequantization 1 prequantization 1 |\
+  --ec_type 3  --max_length 213 --addec 1 --daev2 1  --alpha 90 --prequantization 1 prequantization 1 |\
+  --ec_type 3  --max_length 208 --daev2 1 --alpha 90 --prequantization 1 prequantization 1 "
 # Split the config string into an array using '|' as a delimiter
 IFS='|' read -ra config_array <<< "$configs"
 
