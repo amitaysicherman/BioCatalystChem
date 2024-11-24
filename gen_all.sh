809623706fe44d76adefb4f53a2aa72c7d1a4d09
amitay.s <amitay.sich@gmail.com>
2024-11-24 15:55:04 +0200

    start

--- gen_all.sh
+++ gen_all.sh
@@ -14,7 +14,7 @@ RUN_NAMES=($(ls -d $RESULTS_DIR*/ | xargs -n 1 basename))
 if [ $SLURM_ARRAY_TASK_ID -le ${#RUN_NAMES[@]} ]; then\
     RUN_NAME=${RUN_NAMES[$SLURM_ARRAY_TASK_ID - 1]} # Arrays are 0-indexed
     if [[ $RUN_NAME == *"mix"* ]]; then
-      python eval_gen.py --run_name $RUN_NAME --fast 0 --split test --dups 0 --per_level 1 --res_base ${RESULTS_DIR}  --bs 16 --drop_short 1 --per_ds 1
+      python eval_gen.py --run_name $RUN_NAME --fast 0 --split test --dups 0 --res_base ${RESULTS_DIR}  --bs 16
 #      python eval_gen.py --run_name $RUN_NAME --fast 0 --split valid --dups 0 --per_level 1 --per_ds 1 --res_base ${RESULTS_DIR}  --bs 16
 #      python eval_gen.py --run_name $RUN_NAME --fast 0 --split valid --dups 0 --per_level 0 --res_base ${RESULTS_DIR}  --bs 16  --only_new 1
 #      python eval_gen.py --run_name $RUN_NAME --fast 0 --split valid --dups 1 --per_level 0 --res_base ${RESULTS_DIR}  --bs 16
