1652d3582987af7a8376854a42650f2cbb3bc0a7
amitay.s <amitay.sich@gmail.com>
2024-11-24 11:00:29 +0200

    start

--- train_all.sh
+++ train_all.sh
@@ -6,12 +6,12 @@
 #SBATCH -c 8
 #SBATCH --array=1-6
 
-configs=" --ec_type 3  --max_length 213 --addec 1 --daev2 1  --alpha 50 --prequantization 1 prequantization 1 |\
-  --ec_type 3  --max_length 208 --daev2 1  --alpha 50 --prequantization 1 prequantization 1 |\
-  --ec_type 3  --max_length 213 --addec 1 --daev2 1 --alpha 10 --prequantization 1 prequantization 1 |\
-  --ec_type 3  --max_length 208 --daev2 1  --alpha 10 --prequantization 1 prequantization 1 |\
-  --ec_type 3  --max_length 213 --addec 1 --daev2 1  --alpha 90 --prequantization 1 prequantization 1 |\
-  --ec_type 3  --max_length 208 --daev2 1 --alpha 90 --prequantization 1 prequantization 1 "
+configs=" --ec_type 3  --max_length 213 --addec 1 --daev2 1  --alpha 50 --prequantization 1 |\
+  --ec_type 3  --max_length 208 --daev2 1  --alpha 50 --prequantization 1  |\
+  --ec_type 3  --max_length 213 --addec 1 --daev2 1 --alpha 10 --prequantization 1 |\
+  --ec_type 3  --max_length 208 --daev2 1  --alpha 10 --prequantization 1  |\
+  --ec_type 3  --max_length 213 --addec 1 --daev2 1  --alpha 90 --prequantization 1  |\
+  --ec_type 3  --max_length 208 --daev2 1 --alpha 90 --prequantization 1 "
 # Split the config string into an array using '|' as a delimiter
 IFS='|' read -ra config_array <<< "$configs"
 
