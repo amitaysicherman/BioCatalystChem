275bd720e0dcffce64380c339461927041417453
amitay.s <amitay.sich@gmail.com>
2024-11-24 16:45:38 +0200

    start

--- gen_all.sh
+++ gen_all.sh
@@ -6,19 +6,19 @@
 #SBATCH --gres=gpu:1
 #SBATCH --requeue
 
-# Get the list of directories in results/
+# Get the list of directories in results/ containing "mix"
 RESULTS_DIR="results/"
-RUN_NAMES=($(ls -d $RESULTS_DIR*/ | xargs -n 1 basename))
+RUN_NAMES=($(ls -d $RESULTS_DIR*/ | xargs -n 1 basename | grep "mix"))
 
-# Check if the SLURM_ARRAY_TASK_ID is within the bounds of the available run names
-if [ $SLURM_ARRAY_TASK_ID -le ${#RUN_NAMES[@]} ]; then\
+# Check if the SLURM_ARRAY_TASK_ID is within the bounds of the filtered run names
+if [ $SLURM_ARRAY_TASK_ID -le ${#RUN_NAMES[@]} ]; then
     RUN_NAME=${RUN_NAMES[$SLURM_ARRAY_TASK_ID - 1]} # Arrays are 0-indexed
-    if [[ $RUN_NAME == *"mix"* ]]; then
-      python eval_gen.py --run_name $RUN_NAME --fast 0 --split test --dups 0 --res_base ${RESULTS_DIR}  --bs 16
-#      python eval_gen.py --run_name $RUN_NAME --fast 0 --split valid --dups 0 --per_level 1 --per_ds 1 --res_base ${RESULTS_DIR}  --bs 16
-#      python eval_gen.py --run_name $RUN_NAME --fast 0 --split valid --dups 0 --per_level 0 --res_base ${RESULTS_DIR}  --bs 16  --only_new 1
-#      python eval_gen.py --run_name $RUN_NAME --fast 0 --split valid --dups 1 --per_level 0 --res_base ${RESULTS_DIR}  --bs 16
-    fi
+
+    python eval_gen.py --run_name $RUN_NAME --fast 0 --split test --dups 0 --res_base ${RESULTS_DIR} --bs 16
+# Uncomment the following lines if you want to run these additional evaluations
+#    python eval_gen.py --run_name $RUN_NAME --fast 0 --split valid --dups 0 --per_level 1 --per_ds 1 --res_base ${RESULTS_DIR} --bs 16
+#    python eval_gen.py --run_name $RUN_NAME --fast 0 --split valid --dups 0 --per_level 0 --res_base ${RESULTS_DIR} --bs 16 --only_new 1
+#    python eval_gen.py --run_name $RUN_NAME --fast 0 --split valid --dups 1 --per_level 0 --res_base ${RESULTS_DIR} --bs 16
 
 else
     echo "Error: SLURM_ARRAY_TASK_ID is out of bounds."
