f3eb34db0337644fc43d868b532384094b8dd2e7
amitay.s <amitay.sich@gmail.com>
2024-11-25 16:21:21 +0200

    eval gen

--- train_all.sh
+++ train_all.sh
@@ -4,7 +4,7 @@
 #SBATCH --requeue
 #SBATCH --gres=gpu:A40:1
 #SBATCH -c 8
-#SBATCH --array=1-8
+#SBATCH --array=1-14
 
 configs=" --ec_type 0 |\
   --ec_type 1 --max_length 205 |\
@@ -13,7 +13,13 @@ configs=" --ec_type 0 |\
   --ec_type 3  --max_length 200|\
   --ec_type 3  --max_length 205 --addec 1|\
   --ec_type 3  --max_length 205 --addec 1 --alpha 10|\
-  --ec_type 3  --max_length 205 --addec 1 --alpha 90"
+  --ec_type 3  --max_length 205 --addec 1 --alpha 90|\
+  --ec_type 2 --max_length 208 --prequantization 1|\
+  --ec_type 2 --max_length 213 --addec 1  --prequantization 1|\
+  --ec_type 3 --max_length 208 --prequantization 1|\
+  --ec_type 3 --max_length 213 --addec 1  --prequantization 1|\
+  --ec_type 3 --max_length 213 --addec 1  --prequantization 1 --alpha 10|\
+  --ec_type 3 --max_length 213 --addec 1  --prequantization 1 --alpha 90"
 
 # Split the config string into an array using '|' as a delimiter
 IFS='|' read -ra config_array <<< "$configs"
