5a575b06bf53ce31ad0096a3726823aadebf69d2
amitay.s <amitay.sich@gmail.com>
2024-11-26 11:30:16 +0200

    eval gen

--- vis/pymol_prot_mols.py
+++ vis/pymol_prot_mols.py
@@ -20,12 +20,11 @@ def create_pymol_script_with_sdf(pdb_file: str, sdf_files: list, color_values,
     with open(output_script, 'w') as f:
         f.write(f"load {pdb_file}, protein\n")
         r, g, b = TAB10_COLORS[0]
-        f.write(f"set_color m_color, [{r}, {g}, {b}]\n")
         for i, sdf_file in enumerate(sdf_files):
             molecule_name = f"molecule_{i + 1}"
             f.write(f"load {sdf_file}, {molecule_name}\n")
             f.write(f"show sticks, {molecule_name}\n")
-            f.write(f"color m_color,{molecule_name} \n")
+            f.write(f"color red,{molecule_name} \n")
         for (chain_id, res_num), value in zip(residue_ids, color_values):
             r, g, b, _ = v_cmap(value)
             f.write(f"set_color color_{chain_id}_{res_num}, [{r}, {g}, {b}]\n")
