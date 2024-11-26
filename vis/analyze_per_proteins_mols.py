7620c29def3931eaeb35a646a46210f115b187d8
amitay.s <amitay.sich@gmail.com>
2024-11-26 12:48:17 +0200

    eval gen

--- vis/analyze_per_proteins_mols.py
+++ vis/analyze_per_proteins_mols.py
@@ -1,6 +1,7 @@
 import os
 from vis.utils import load_maps, remove_dup_mis_mols, filter_molecule_by_len
 import glob
+
 id_to_smile, smile_to_id, uniport_to_ec = load_maps()
 
 base_dir = "datasets/docking2"
@@ -11,6 +12,10 @@ for protein_id in os.listdir(base_dir):
     m_dock_positions = []
     for m in molecules_ids:
         sdf_files = glob.glob(f"datasets/docking2/{protein_id}/{m}/complex_0/*.sdf")
-        sdf_files = filter_molecule_by_len(sdf_files, 0.5)
-        m_dock_positions.append(len(sdf_files))
-    print(f"Protein {protein_id} - {len(molecules_ids)} molecules, {m_dock_positions}")
+        try:
+            sdf_files = filter_molecule_by_len(sdf_files, 0.5)
+            m_dock_positions.append(len(sdf_files))
+        except Exception as e:
+            print(f"Error in {protein_id} - {m}", e)
+    if len(molecules_ids) > 5 and len(molecules_ids) < 10 and sum([x > 2 for x in m_dock_positions]) > 3:
+        print(f"Protein {protein_id} - {len(molecules_ids)} molecules, {m_dock_positions}")
