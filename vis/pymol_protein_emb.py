fb0b08f76dbee492bfab61db0e43400685530de4
amitay.s <amitay.sich@gmail.com>
2024-11-26 11:12:44 +0200

    eval gen

--- vis/pymol_protein_emb.py
+++ vis/pymol_protein_emb.py
@@ -4,15 +4,15 @@ from sklearn.preprocessing import MinMaxScaler
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 
-from vis.utils import get_residue_ids_from_pdb,replace_local_pathes
+from vis.utils import get_residue_ids_from_pdb, replace_local_pathes
 
-def create_pymol_script(pdb_file: str, embedding_file: str, output_script,k_clusters=None):
+
+def create_pymol_script(pdb_file: str, embedding_file: str, output_script, k_clusters=None):
     embeddings = np.load(embedding_file)[1:-1]  # Assuming shape is (num_residues, embedding_dimension)
     residue_ids = get_residue_ids_from_pdb(pdb_file)
     assert len(residue_ids) == embeddings.shape[0], "Mismatch between PDB residues and embeddings"
 
-
-    if k_clusters is not None:
+    if k_clusters:
         kmeans = KMeans(n_clusters=k_clusters)
         kmeans.fit(embeddings)
         cluster_labels = kmeans.labels_
@@ -30,26 +30,26 @@ def create_pymol_script(pdb_file: str, embedding_file: str, output_script,k_clus
         emb_colors = viridis_colors(normalized_pca_values)
         emb_colors = emb_colors[:, :3]
 
-
     with open(output_script, 'w') as f:
         # Load the PDB file in PyMOL
         f.write(f"load {pdb_file}, protein\n")
         for (chain_id, res_num), color in zip(residue_ids, emb_colors):
-            r, g, b= color  # RGB values
+            r, g, b = color  # RGB values
             f.write(f"set_color color_{chain_id}_{res_num}, [{r}, {g}, {b}]\n")
             f.write(f"color color_{chain_id}_{res_num}, protein and chain {chain_id} and resi {res_num}\n")
         f.write('show_as("mesh"      ,"all")')
     print(f"PyMOL script '{output_script}' created successfully.")
 
 
-# Example usage
-output_script = "vis/scripts/protein_emb.pml"
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--k", type=int, default=0)
+args = parser.parse_args()
+k = args.k
+output_script = f"vis/scripts/protein_emb_{k}.pml"
 create_pymol_script(
     "datasets/pdb_files/A0A009H5L7/A0A009H5L7_esmfold.pdb",
     "datasets/docking/A0A009H5L7/protein.npy",
     output_script=output_script)
 
 replace_local_pathes(output_script)
-
-
-
