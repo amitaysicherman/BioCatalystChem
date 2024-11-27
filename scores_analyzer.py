9c3bb1152c78f9d53631cb4095e8609737f81377
amitay.s <amitay.sich@gmail.com>
2024-11-27 13:01:18 +0200

    eval gen

--- scores_analyzer.py
+++ scores_analyzer.py
@@ -1,19 +1,21 @@
 import os
+
 import pandas as pd
 from tqdm import tqdm
+
 from preprocessing.sample_tagging import SampleTags
-from dataclasses import dataclass
 
 
-def get_scores_from_file(file_path, indexes):
-    df = pd.read_csv(file_path, index_col=0, header=None, names=["index", "res"],
-                     dtype={"res": int, "index": int})
-    print(len(df))
+def get_scores_from_df(df, indexes):
     if indexes is not None:
         indexes_in_df = [x for x in df.index if x in indexes]
         df = df.loc[indexes_in_df]
-    print(len(df))
     return df["res"].mean()
+
+
+def read_file(file_path):
+    return pd.read_csv(file_path, index_col=0, header=None, names=["index", "res"],
+                       dtype={"res": int, "index": int})
 
 
 def file_name_to_split_n_k(file_name):
@@ -28,6 +30,7 @@ def config_to_file_name(split, n, k):
 
 class ValTestFiles:
     def __init__(self, run_name):
+        self.run_name = run_name
         self.base_dir = f"results/{run_name}"
 
         all_files = [x for x in os.listdir(self.base_dir) if x.endswith(".txt")]
@@ -36,7 +39,10 @@ class ValTestFiles:
         test_n_k = [(n, k) for split, n, k in all_split_n_k if split == "test"]
         n_k = set(valid_n_k).intersection(test_n_k)
         self.configs = n_k
-        self.all_k = set([k for n, k in n_k])
+        self.valid_config_to_df = {c: read_file(os.path.join(self.base_dir, config_to_file_name("valid", c[0], c[1])))
+                                   for c in n_k}
+        self.test_config_to_df = {c: read_file(os.path.join(self.base_dir, config_to_file_name("test", c[0], c[1]))) for
+                                  c in n_k}
 
     def get_k_configs(self, k):
         return [c for c in self.configs if c[1] == k]
@@ -46,40 +52,67 @@ class ValTestFiles:
         test_scores = []
         configs = self.get_k_configs(k)
         for c in configs:
-            n, k = c
-            valid_file = config_to_file_name("valid", n, k)
-            valid_scores.append(get_scores_from_file(os.path.join(self.base_dir, valid_file), index_valid))
-            test_file = config_to_file_name("test", n, k)
-            test_scores.append(get_scores_from_file(os.path.join(self.base_dir, test_file), index_test))
+            valid_scores.append(get_scores_from_df(self.valid_config_to_df[c], index_valid))
+            test_scores.append(get_scores_from_df(self.test_config_to_df[c], index_test))
         best_valid = max(valid_scores)
         best_test = test_scores[valid_scores.index(best_valid)]
         return best_test
 
-    def get_score(self, index_valid, index_test):
-        return {k: self.get_score_k(k, index_valid, index_test) for k in self.all_k}
+
+def impact_score(results: pd.Series):
+    base_lines_indexes = [x for x in results.index if "paper" in x or "regular" in x]
+    pre_indexes = [x for x in results.index if "pre" in x]
+    dae = [x for x in results.index if "dae" in x]
+    base_score = results.loc[base_lines_indexes].max()
+    pre_score = results.loc[pre_indexes].max()
+    dae_score = results.loc[dae].max()
+    return dae_score - max(base_score, pre_score)
 
 
-sample_tags_valid = SampleTags(split="valid")
-sample_tags_test = SampleTags(split="test")
-
-filter_tags = [("ds", lambda x: x == "rhea_reaction_smiles"), ("num_train_tgt", lambda x: x == 0),
-               ("num_large_mol", lambda x: x > 2)]
-index_valid = sample_tags_valid.get_query_indexes(filter_tags)
-index_test = sample_tags_test.get_query_indexes(filter_tags)
-print(len(index_valid), len(index_test))
 all_methods = os.listdir("results")
 all_methods = [x for x in all_methods if os.path.isdir(os.path.join("results", x))]
 print(all_methods)
-all_results = dict()
+run_num_to_obj = dict()
 for name in tqdm(all_methods):
     if name == "full":
         continue
     if len([x for x in os.listdir(os.path.join("results", name)) if x.endswith(".txt")]) == 0:
         continue
-    res = ValTestFiles(name).get_score_k(5, index_valid, index_test)
+    run_num_to_obj[name] = ValTestFiles(name)
 
-    all_results[name] = res
-    # all_results.append(res)
+sample_tags_valid = SampleTags(split="valid")
+sample_tags_test = SampleTags(split="test")
+
+filter_configs = [
+    [("ds", lambda x: x == "rhea_reaction_smiles"), ("num_train_tgt", lambda x: x == 0),
+     ("num_large_mol", lambda x: x > 2)],
+    [("ds", lambda x: x == "rhea_reaction_smiles"), ("num_train_tgt", lambda x: x == 0),
+     ("num_large_mol", lambda x: x > 2), ("num_mol", lambda x: x > 2)],
+    [("ds", lambda x: x == "rhea_reaction_smiles"), ("num_train_tgt", lambda x: x == 0),
+     ("num_large_mol", lambda x: x > 2), ("num_mol", lambda x: x > 2), ("ec_l_1", lambda x: x == "1.1")],
+    [("ds", lambda x: x == "rhea_reaction_smiles"), ("num_train_tgt", lambda x: x == 0),
+     ("num_large_mol", lambda x: x > 2), ("num_mol", lambda x: x > 2), ("ec_l_1", lambda x: x == "1.1"),
+     ("ec_l_2", lambda x: x == "1.1.1")],
+]
+all_results = []
+for filter_tags in filter_configs:
+    index_valid = sample_tags_valid.get_query_indexes(filter_tags)
+    index_test = sample_tags_test.get_query_indexes(filter_tags)
+    config_results = dict()
+    for name, obj in run_num_to_obj.items():
+        config_results[name] = obj.get_score_k(5, index_valid, index_test)
+    config_results['count'] = len(index_test)
+    config_results["impact"] = impact_score(pd.Series(config_results))
+    results = pd.Series(config_results)
+    all_results.append(results)
 print(all_results)
-results = pd.Series(all_results)
+#convert to dataframe
+df = pd.DataFrame(all_results)
+print(df)
+df.to_csv("results/impact_results.csv")
+
+config_results[name] = res
+# all_results.append(res)
+print(config_results)
+results = pd.Series(config_results)
 print(results)
