94aaf1f7ab0d3b82a0834c242efca4060dd27613
amitay.s <amitay.sich@gmail.com>
2024-11-27 11:23:16 +0200

    eval gen

--- scores_analyzer.py
+++ scores_analyzer.py
@@ -38,28 +38,30 @@ class ValTestFiles:
     def get_k_configs(self, k):
         return [c for c in self.configs if c[1] == k]
 
-    def get_score_k(self, k, indexes=None):
+    def get_score_k(self, k, index_valid, index_test):
         valid_scores = []
         test_scores = []
         configs = self.get_k_configs(k)
         for c in configs:
             n, k = c
             valid_file = config_to_file_name("valid", n, k)
-            valid_scores.append(get_scores_from_file(os.path.join(self.base_dir, valid_file), indexes))
+            valid_scores.append(get_scores_from_file(os.path.join(self.base_dir, valid_file), index_valid))
             test_file = config_to_file_name("test", n, k)
-            test_scores.append(get_scores_from_file(os.path.join(self.base_dir, test_file), indexes))
+            test_scores.append(get_scores_from_file(os.path.join(self.base_dir, test_file), index_test))
         best_valid = max(valid_scores)
         best_test = test_scores[valid_scores.index(best_valid)]
         return best_test
 
-    def get_score(self, indexes=None):
-        return {k: self.get_score_k(k, indexes) for k in self.all_k}
+    def get_score(self, index_valid, index_test):
+        return {k: self.get_score_k(k, index_valid, index_test) for k in self.all_k}
 
 
-sample_tags = SampleTags(split="valid")
-indxs = sample_tags.get_query_indexes([("num_mol", lambda x: x > 3)])
-print(len(indxs), len(sample_tags.df))
-
+sample_tags_valid = SampleTags(split="valid")
+sample_tags_test = SampleTags(split="test")
+filter_tags = [("num_mol", lambda x: x > 3)]
+indxs_valid = sample_tags_valid.get_query_indexes(filter_tags)
+index_test = sample_tags_test.get_query_indexes(filter_tags)
+print(len(indxs_valid), len(index_test))
 all_methods = os.listdir("results")
 all_methods = [x for x in all_methods if os.path.isdir(os.path.join("results", x))]
 print(all_methods)
@@ -69,7 +71,7 @@ for name in tqdm(all_methods):
         continue
     if len([x for x in os.listdir(os.path.join("results", name)) if x.endswith(".txt")]) == 0:
         continue
-    res = ValTestFiles(name).get_score_k(5, indxs)
+    res = ValTestFiles(name).get_score_k(5, index_valid, index_test)
 
     all_results[name] = res
     # all_results.append(res)
