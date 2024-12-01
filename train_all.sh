5a5476beebf4c00cad11d6b3471bfd53963408cc
amitay.s <amitay.sich@gmail.com>
2024-12-01 21:34:54 +0200

    v2

--- train_all.sh
+++ train_all.sh
@@ -4,25 +4,21 @@
 #SBATCH --requeue
 #SBATCH --gres=gpu:L40:1
 #SBATCH -c 8
-#SBATCH --array=1-14
+#SBATCH --array=1-10
 
-configs=" --ec_type 0 |\
-  --ec_type 1 --max_length 205 |\
-  --ec_type 2 --max_length 200 |\
-  --ec_type 2  --max_length 205 --addec 1|\
-  --ec_type 3  --max_length 200|\
-  --ec_type 3  --max_length 205 --addec 1|\
-  --ec_type 3  --max_length 205 --addec 1 --alpha 10|\
-  --ec_type 3  --max_length 205 --addec 1 --alpha 90|\
-  --ec_type 2 --max_length 208 --prequantization 1|\
-  --ec_type 2 --max_length 213 --addec 1  --prequantization 1|\
-  --ec_type 3 --max_length 208 --prequantization 1|\
-  --ec_type 3 --max_length 213 --addec 1  --prequantization 1|\
-  --ec_type 3 --max_length 213 --addec 1  --prequantization 1 --alpha 10|\
-  --ec_type 3 --max_length 213 --addec 1  --prequantization 1 --alpha 90"
+configs=" --ec_type 0 --daa_type 0 --add_ec 0|\
+    --ec_type 1 --daa_type 0 --add_ec 0|\
+    --ec_type 2 --daa_type 0 --add_ec 0|\
+    --ec_type 2 --daa_type 0 --add_ec 1|\
+    --ec_type 2 --daa_type 1 --add_ec 0|\
+    --ec_type 2 --daa_type 1 --add_ec 1|\
+    --ec_type 2 --daa_type 2 --add_ec 0|\
+    --ec_type 2 --daa_type 2 --add_ec 1|\
+    --ec_type 2 --daa_type 3 --add_ec 0|\
+    --ec_type 2 --daa_type 3 --add_ec 1"
 
 # Split the config string into an array using '|' as a delimiter
 IFS='|' read -ra config_array <<< "$configs"
 
 config=${config_array[$((SLURM_ARRAY_TASK_ID - 1))]}
-python finetune_ecreact.py $config
+python finetune_ecreact_v2.py $config
